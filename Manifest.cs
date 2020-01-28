0fddb7a5d1800c06e05e64e82fce7cd36ea24bad
Peter Keating <peter@moov2.com>
2020-01-28 21:06:10 +0000

    Set up project using module boilerplate

---
+++ Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Author = "Etch UK Ltd.",
+    Category = "Security",
+    Description = "Module for Orchard Core to enable configuring security per content item.",
+    Name = "Secured Content",
+    Version = "0.0.1",
+    Website = "https://etchuk.com"
+)]
---
+++ README.md
@@ -0,0 +1,7 @@
+# Etch.OrchardCore.SecuredContent
+
+Module for Orchard Core to enable configuring security per content item.
+
+## Orchard Core Reference
+
+This module is referencing the RC1 build of Orchard Core ([`1.0.0-rc1-10004`](https://www.nuget.org/packages/OrchardCore.Module.Targets/1.0.0-rc1-10004)).
