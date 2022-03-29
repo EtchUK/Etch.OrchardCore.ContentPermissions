6dcb4cd3b8897690be094fd5ab2db044a1cb2cf2
Peter Keating <pete.keating@etchuk.com>
2022-03-29 16:01:09 +0100

    Store enabled & selected roles when part is indexed

---
+++ Indexing/ContentPermissionsPartIndexHandler.cs
@@ -0,0 +1,21 @@
+﻿using Etch.OrchardCore.ContentPermissions.Models;
+using OrchardCore.Indexing;
+using System.Threading.Tasks;
+
+namespace Etch.OrchardCore.ContentPermissions.Indexing
+{
+    public class ContentPermissionsPartIndexHandler : ContentPartIndexHandler<ContentPermissionsPart>
+    {
+        public override Task BuildIndexAsync(ContentPermissionsPart part, BuildPartIndexContext context)
+        {
+            context.DocumentIndex.Set($"{nameof(ContentPermissionsPart)}.{nameof(ContentPermissionsPart.Enabled)}", part.Enabled, DocumentIndexOptions.Store);
+
+            foreach (var role in part.Roles)
+            {
+                context.DocumentIndex.Set($"{nameof(ContentPermissionsPart)}.{nameof(ContentPermissionsPart.Roles)}", role, DocumentIndexOptions.Store);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
